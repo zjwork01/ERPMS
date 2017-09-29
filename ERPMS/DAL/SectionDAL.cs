37245849d6147a4ef39e163b3d2f18792d9c975e
zj <zj_rest@126.com>
2017-09-29 16:30:53 +0800

    部门数据操作类

---
+++ ERPMS/DAL/SectionDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**
+*作者：赵进
+*时间：2017/9/29 16:28:56
+*描述：部门数据操作类
+*版本：1.0.0
+*/
+namespace DAL
+{
+    public class SectionDAL
+    {
+
+    }
+}
