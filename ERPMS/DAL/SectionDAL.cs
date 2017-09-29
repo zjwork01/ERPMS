d16b99894fb9573fd90b648e3fed0eb6482ee70c
zj <zj_rest@126.com>
2017-09-29 18:05:06 +0800

    SectionDAL

--- ERPMS/DAL/SectionDAL.cs
+++ ERPMS/DAL/SectionDAL.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using System.Data.SqlClient;
 
 /**
 *作者：赵进
@@ -14,6 +17,36 @@ namespace DAL
 {
     public class SectionDAL
     {
-
+        /// <summary>
+        /// 添加部门信息
+        /// </summary>
+        /// <param name="section">部门</param>
+        /// <returns>返回受影响的行数</returns>
+        public bool Add(Section section)
+        {
+            try
+            {
+                string sql = "insert into section(s_name,s_owner,s_tel,s_note) values (@name,@owner,@tel,@note)";
+                SqlParameter[] para = new SqlParameter[]{
+                    new SqlParameter("@name",section.S_name),
+                    new SqlParameter("@owner",section.S_owner),
+                    new SqlParameter("@tel",section.S_tel),
+                    new SqlParameter("@note",section.S_note)
+                };
+                int n = SQLHelper.ExecuteNonQuery(SQLHelper.GetConnection(), System.Data.CommandType.Text, sql, para);
+                if (n > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
