b537574690826258285f07cda68653cacf7f6fc3
zj <zj_rest@126.com>
2017-09-29 12:00:05 +0800

    添加实体类

---
+++ ERPMS/Model/Material.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 原材料
+    /// </summary>
+    public class Material
+    {
+        private int m_id;
+        /// <summary>
+        /// 编号
+        /// </summary>
+        public int Id
+        {
+            get { return m_id; }
+            set { m_id = value; }
+        }
+        private string m_name;
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+        private string m_unit;
+        /// <summary>
+        /// 计量单位
+        /// </summary>
+        public string Unit
+        {
+            get { return m_unit; }
+            set { m_unit = value; }
+        }
+        private double m_price;
+        /// <summary>
+        /// 价格
+        /// </summary>
+        public double Price
+        {
+            get { return m_price; }
+            set { m_price = value; }
+        }
+        private int m_number;
+        /// <summary>
+        /// 现有数量
+        /// </summary>
+        public int Number
+        {
+            get { return m_number; }
+            set { m_number = value; }
+        }
+        private int m_warehouse;
+        /// <summary>
+        /// 所属仓库
+        /// </summary>
+        public int Warehouse
+        {
+            get { return m_warehouse; }
+            set { m_warehouse = value; }
+        }
+        private int m_provider;
+        /// <summary>
+        /// 供应商
+        /// </summary>
+        public int Provider
+        {
+            get { return m_provider; }
+            set { m_provider = value; }
+        }
+        private string m_note;
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Note
+        {
+            get { return m_note; }
+            set { m_note = value; }
+        }
+    }
+}
---
+++ ERPMS/Model/Provider.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    /// <summary>
+    /// 供应商
+    /// </summary>
+    public class Provider
+    {
+        private int p_id;
+        /// <summary>
+        /// 供应商编号
+        /// </summary>
+        public int P_id
+        {
+            get { return p_id; }
+            set { p_id = value; }
+        }
+        private string p_company;
+        /// <summary>
+        /// 供应商公司名称
+        /// </summary>
+        public string P_company
+        {
+            get { return p_company; }
+            set { p_company = value; }
+        }
+        private string p_manager;
+        /// <summary>
+        /// 负责人
+        /// </summary>
+        public string P_manager
+        {
+            get { return p_manager; }
+            set { p_manager = value; }
+        }
+        private string p_tel;
+        /// <summary>
+        /// 供应商联系方式
+        /// </summary>
+        public string P_tel
+        {
+            get { return p_tel; }
+            set { p_tel = value; }
+        }
+        private string p_cardname;
+        /// <summary>
+        /// 与供应商进行交易的银行名称
+        /// </summary>
+        public string P_cardname
+        {
+            get { return p_cardname; }
+            set { p_cardname = value; }
+        }
+        private string card;
+        /// <summary>
+        /// 与供应商进行交易的银行卡号
+        /// </summary>
+        public string Card
+        {
+            get { return card; }
+            set { card = value; }
+        }
+    }
+}
